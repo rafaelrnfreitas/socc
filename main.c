31d55977cf0ebc5a385703bdd6708a1a76b84138
Rafael Freitas <rafarnf2008@gmail.com>
2026-05-31 23:36:09 -0300

    Replace debug output with actual tokenization

--- main.c
+++ main.c
@@ -18,10 +18,10 @@ typedef enum {
 
 typedef struct {
     TokenType type;
-    const char* value;
+    char* value;
 } Token;
 
-Token* Tokenize(const char* str, size_t fsize);
+Token* Tokenize(const char* str, size_t fsize, size_t* outCount);
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
     }
     
     size_t size = ftell(fptr);
-    char* buffer = malloc(size);
+    char* buffer = malloc(size + 1);
 
     if(buffer == NULL) {
         fclose(fptr);
@@ -53,52 +53,69 @@ int main(int argc, char *argv[]) {
     fseek(fptr, 0, SEEK_SET);
 
     fread(buffer, sizeof(char), size, fptr);
-    Tokenize(buffer, size);
+    buffer[size] = '\0';
 
+    size_t tokenCount = 0;
+    Token* tokens = Tokenize(buffer, size, &tokenCount);
+    
+    for(int i = 0; i < tokenCount; i++) {
+        if(tokens[i].value != NULL) free(tokens[i].value);
+    }
+
+    free(tokens);
     free(buffer);
     fclose(fptr);
 
     return 0;
 }
 
-Token* Tokenize(const char* str, size_t fsize) {
+Token* Tokenize(const char* str, size_t fsize, size_t* outCount) {
+    Token* tokens = malloc(256 * sizeof(Token));
+    int tokCount = 0;
+
     char tok[256];
-    int toklen = 0;
+    size_t toklen = 0;
 
     for(int i = 0; i < fsize; i++) {
         char c = str[i];
 
         if(isalnum(c) || c == '_') {
             tok[toklen++] = c;
-        } else {
-            if(toklen > 0) {
-                tok[toklen + 1] = '\0';
+            continue;
+        }
 
-                if(isdigit(tok[0])) {
-                    printf("found an integer literal\n");
+        if(toklen > 0) {
+            tok[toklen] = '\0';
+
+            if(isdigit(tok[0])) {
+                char* value = malloc(toklen + 1);
+                strcpy(value, tok);
+                tokens[tokCount++] = (Token){.type = TOK_INTLIT, .value = value};
+            } else {
+                if(strcmp(tok, "int") == 0) {
+                    tokens[tokCount++] = (Token){.type = TOK_INT, .value = NULL};
+                } else if(strcmp(tok, "return") == 0) {
+                    tokens[tokCount++] = (Token){.type = TOK_RET, .value = NULL};
                 } else {
-                    if(strcmp(tok, "int") == 0) {
-                        printf("found an integer\n");
-                    } else if(strcmp(tok, "return") == 0) {
-                        printf("found a return\n");
-                    } else {
-                        printf("found an identifier\n");
-                    }
+                    char* value = malloc(toklen + 1);
+                    strcpy(value, tok);
+                    tokens[tokCount++] = (Token){.type = TOK_ID, .value = value};
                 }
-                toklen = 0;
             }
-            
-            if(c == '(') {
-                printf("found a left parentheses\n");
-            } else if(c == ')') {
-                printf("found a right parentheses\n");
-            } else if(c == '{') {
-                printf("found a left brace\n");
-            } else if(c == '}') {
-                printf("found a right brace\n");
-            } else if(c == ';') {
-                printf("found a semicolon\n");
-            }  
+            toklen = 0;
+        }
+
+        switch(c) {
+            case '(': tokens[tokCount++] = (Token){.type = TOK_LPAREN, .value = NULL}; break;
+            case ')': tokens[tokCount++] = (Token){.type = TOK_RPAREN, .value = NULL}; break;
+            case '{': tokens[tokCount++] = (Token){.type = TOK_LBRACE, .value = NULL}; break;
+            case '}': tokens[tokCount++] = (Token){.type = TOK_RBRACE, .value = NULL}; break;
+            case ';': tokens[tokCount++] = (Token){.type = TOK_SEMICOLON, .value = NULL}; break;
         }
     }
+    
+    tokens[tokCount++] = (Token){.type = TOK_EOF, .value = NULL};
+    *outCount = tokCount;
+
+    return tokens;
 }
