760326f5cd4bd2968aabd50e1da9adb5651120fa
Rafael Freitas <rafarnf2008@gmail.com>
2026-06-13 02:57:01 -0300

    Improve tokenization with array of tokens and EmitToken function

--- src/lexer.c
+++ src/lexer.c
@@ -3,6 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+static const struct { char a, b; TokenType type; } twoCharTokens[] = {
+    {'&', '&', TOK_AMP_AMP},
+    {'|', '|', TOK_PIPE_PIPE},
+    {'=', '=', TOK_EQUAL_EQUAL},
+    {'!', '=', TOK_BANG_EQUAL},
+    {'<', '=', TOK_LESS_EQUAL},
+    {'>', '=', TOK_GREATER_EQUAL},
+    {'<', '<', TOK_LESS_LESS},
+    {'>', '>', TOK_GREATER_GREATER},
+};
+
+static const struct { char c; TokenType type; } singleCharTokens[] = {
+    {'(', TOK_OPAREN},
+    {')', TOK_CPAREN},
+    {'{', TOK_OBRACE},
+    {'}', TOK_CBRACE},
+    {';', TOK_SEMICOLON},
+    {'+', TOK_PLUS},
+    {'-', TOK_MINUS},
+    {'*', TOK_STAR},
+    {'/', TOK_SLASH},
+    {'%', TOK_PERCENT},
+    {'!', TOK_BANG},
+    {'~', TOK_TILDE},
+    {'<', TOK_LESS},
+    {'>', TOK_GREATER},
+    {'&', TOK_AMP},
+    {'|', TOK_PIPE},
+    {'^', TOK_CARET},
+};
+
+static void EmitToken(char* tok, size_t toklen, Vector* tokens) {
+    if(toklen == 0) return;
+    tok[toklen] = '\0';
+
+    if(isdigit(tok[0])) {
+        char* value = malloc(toklen + 1);
+        strcpy(value, tok);
+        VectorPush(&(Token){.type = TOK_INTLIT, .value = value}, tokens);
+        return;
+    }
+
+    if(strcmp(tok, "int") == 0) {
+        VectorPush(&(Token){.type = TOK_INT, .value = NULL}, tokens);
+    } else if(strcmp(tok, "return") == 0) {
+        VectorPush(&(Token){.type = TOK_RET, .value = NULL}, tokens);
+    } else {
+        char* value = malloc(toklen + 1);
+        strcpy(value, tok);
+        VectorPush(&(Token){.type = TOK_ID, .value = value}, tokens);
+    }
+}
+
 Vector Tokenize(const char* str, size_t fsize) {
     Vector tokens;
     VectorInit(&tokens, sizeof(Token));
@@ -10,60 +63,25 @@ Vector Tokenize(const char* str, size_t fsize) {
     char tok[256];
     size_t toklen = 0;
 
-    for(int i = 0; i < fsize; i++) {
+    for(size_t i = 0; i < fsize; i++) {
         char c = str[i];
 
         if(i + 1 < fsize) {
             char next = str[i + 1];
+            int matched = 0;
 
-            if(c == '&' && next == '&') {
-                VectorPush(&(Token){.type = TOK_AMP_AMP, .value = NULL}, &tokens);
-                i++;
-                continue;
+            for(size_t j = 0; j < sizeof(twoCharTokens) / sizeof(twoCharTokens[0]); j++) {
+                if(c == twoCharTokens[j].a && next == twoCharTokens[j].b) {
+                    EmitToken(tok, toklen, &tokens);
+                    toklen = 0;
+                    VectorPush(&(Token){.type = twoCharTokens[j].type, .value = NULL}, &tokens);
+                    i++;
+                    matched = 1;
+                    break;
+                }
             }
 
-            if(c == '|' && next == '|') {
-                VectorPush(&(Token){.type = TOK_PIPE_PIPE, .value = NULL}, &tokens);
-                i++;
-                continue;
-            }
-
-            if(c == '=' && next == '=') {
-                VectorPush(&(Token){.type = TOK_EQUAL_EQUAL, .value = NULL}, &tokens);
-                i++;
-                continue;
-            }
-
-            if(c == '!' && next == '=') {
-                VectorPush(&(Token){.type = TOK_BANG_EQUAL, .value = NULL}, &tokens);
-                i++;
-                continue;
-            }
-
-            if(c == '<' && next == '=') {
-                VectorPush(&(Token){.type = TOK_LESS_EQUAL, .value = NULL}, &tokens);
-                i++;
-                continue;
-            }
-
-            if(c == '>' && next == '=') {
-                VectorPush(&(Token){.type = TOK_GREATER_EQUAL, .value = NULL}, &tokens);
-                i++;
-                continue;
-            }
-
-			if(c == '<' && next == '<') {
-                VectorPush(&(Token){.type = TOK_LESS_LESS, .value = NULL}, &tokens);
-				i++;
-				continue;
-			}
-			
-			if(c == '>' && next == '>') {
-                VectorPush(&(Token){.type = TOK_GREATER_GREATER, .value = NULL}, &tokens);
-				i++;
-				continue;
-			} 
-			
+            if(matched) continue;
         }
 
         if(isalnum(c) || c == '_') {
@@ -71,72 +89,18 @@ Vector Tokenize(const char* str, size_t fsize) {
             continue;
         }
 
-        if(toklen > 0) {
-            tok[toklen] = '\0';
+        EmitToken(tok, toklen, &tokens);
+        toklen = 0;
 
-            if(isdigit(tok[0])) {
-                char* value = malloc(toklen + 1);
-                strcpy(value, tok);
-                VectorPush(&(Token){.type = TOK_INTLIT, .value = value}, &tokens);
-            } else {
-                if(strcmp(tok, "int") == 0) {
-                    VectorPush(&(Token){.type = TOK_INT, .value = NULL}, &tokens);
-                } else if(strcmp(tok, "return") == 0) {
-                    VectorPush(&(Token){.type = TOK_RET, .value = NULL}, &tokens);
-                } else {
-                    char* value = malloc(toklen + 1);
-                    strcpy(value, tok);
-                    VectorPush(&(Token){.type = TOK_ID, .value = value}, &tokens);
-                }
-            }
-
-            toklen = 0;
-        }
-
-        switch(c) {
-            case '(': VectorPush(&(Token){.type = TOK_OPAREN, .value = NULL}, &tokens); break;
-            case ')': VectorPush(&(Token){.type = TOK_CPAREN, .value = NULL}, &tokens); break;
-            case '{': VectorPush(&(Token){.type = TOK_OBRACE, .value = NULL}, &tokens); break;
-            case '}': VectorPush(&(Token){.type = TOK_CBRACE, .value = NULL}, &tokens); break;
-            case ';': VectorPush(&(Token){.type = TOK_SEMICOLON, .value = NULL}, &tokens); break;
-
-            case '+': VectorPush(&(Token){.type = TOK_PLUS, .value = NULL}, &tokens); break;
-            case '-': VectorPush(&(Token){.type = TOK_MINUS, .value = NULL}, &tokens); break;
-            case '*': VectorPush(&(Token){.type = TOK_STAR, .value = NULL}, &tokens); break;
-            case '/': VectorPush(&(Token){.type = TOK_SLASH, .value = NULL}, &tokens); break;
-			case '%': VectorPush(&(Token){.type = TOK_PERCENT, .value = NULL}, &tokens); break;
-
-            case '!': VectorPush(&(Token){.type = TOK_BANG, .value = NULL}, &tokens); break;
-            case '~': VectorPush(&(Token){.type = TOK_TILDE, .value = NULL}, &tokens); break;
-            case '<': VectorPush(&(Token){.type = TOK_LESS, .value = NULL}, &tokens); break;
-            case '>': VectorPush(&(Token){.type = TOK_GREATER, .value = NULL}, &tokens); break;
-			
-			case '&': VectorPush(&(Token){.type = TOK_AMP, .value = NULL}, &tokens); break;
-			case '|': VectorPush(&(Token){.type = TOK_PIPE, .value = NULL}, &tokens); break;
-			case '^': VectorPush(&(Token){.type = TOK_CARET, .value = NULL}, &tokens); break;
-        }
-    }
-
-    if(toklen > 0) {
-        tok[toklen] = '\0';
-
-        if(isdigit(tok[0])) {
-            char* value = malloc(toklen + 1);
-            strcpy(value, tok);
-            VectorPush(&(Token){.type = TOK_INTLIT, .value = value}, &tokens);
-        } else {
-            if(strcmp(tok, "int") == 0) {
-                VectorPush(&(Token){.type = TOK_INT, .value = NULL}, &tokens);
-            } else if(strcmp(tok, "return") == 0) {
-                VectorPush(&(Token){.type = TOK_RET, .value = NULL}, &tokens);
-            } else {
-                char* value = malloc(toklen + 1);
-                strcpy(value, tok);
-                VectorPush(&(Token){.type = TOK_ID, .value = value}, &tokens);
+        for(size_t j = 0; j < sizeof(singleCharTokens) / sizeof(singleCharTokens[0]); j++) {
+            if(c == singleCharTokens[j].c) {
+                VectorPush(&(Token){.type = singleCharTokens[j].type, .value = NULL}, &tokens);
+                break;
             }
         }
     }
 
+    EmitToken(tok, toklen, &tokens);
     VectorPush(&(Token){.type = TOK_EOF, .value = NULL}, &tokens);
 
     return tokens;
